d54e65b26b2891028ec4d8665c90b75082ed1cea
xu3woli <47500703+amgithub07@users.noreply.github.com>
2025-01-25 20:17:01 +0800

    250125-init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="text-primary">Hello Luma</h1>
+    <h2 class="text-lu-primary">Hello Luma</h2>
+    <h3 class="text-lu-accent-dark">Hello Luma</h3>
+    <h4 class="text-lu-accent">Hello Luma</h4>
+
+</template>
+
+<style scoped></style>
